55abf001768d17fde618d8286a8f066411bc17eb
Volkov Aleksei <rohtuasad@gmail.com>
2022-05-16 07:49:57 +0300

    Filling database with initial values on application start

---
+++ core/src/main/java/ru/rohtuasad/chipin/core/bootstrap/DevBootstrap.java
@@ -0,0 +1,26 @@
+package ru.rohtuasad.chipin.core.bootstrap;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import ru.rohtuasad.chipin.core.chat.model.Chat;
+import ru.rohtuasad.chipin.core.chat.service.ChatService;
+
+@Component
+@RequiredArgsConstructor
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+  private final ChatService chatService;
+
+  @SneakyThrows
+  @Override
+  public void onApplicationEvent(@NonNull ContextRefreshedEvent refreshedEvent) {
+    Chat chat = new Chat();
+    chat.setTgChatId("1");
+    chat.setTgChatName("Chat name");
+    chatService.saveChat(chat);
+  }
+}
