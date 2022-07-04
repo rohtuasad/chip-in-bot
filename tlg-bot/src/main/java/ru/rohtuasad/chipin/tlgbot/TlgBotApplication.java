f77deede14f7c950aeac1cdadc54e896e10375cd
Volkov Aleksei <rohtuasad@gmail.com>
2022-07-04 18:40:38 +0300

    Added telegram bot module

---
+++ tlg-bot/src/main/java/ru/rohtuasad/chipin/tlgbot/TlgBotApplication.java
@@ -0,0 +1,13 @@
+package ru.rohtuasad.chipin.tlgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlgBotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TlgBotApplication.class, args);
+  }
+
+}
---
+++ tlg-bot/src/test/java/ru/rohtuasad/chipin/tlgbot/TlgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rohtuasad.chipin.tlgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlgBotApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
