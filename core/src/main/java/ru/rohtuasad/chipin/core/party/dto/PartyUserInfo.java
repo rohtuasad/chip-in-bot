407fcbd2da812c8a50e985cc76b57a39b8f93594
Volkov Aleksei <rohtuasad@gmail.com>
2022-10-14 23:08:37 +0300

    Added transfers and payments in party information

--- core/src/main/java/ru/rohtuasad/chipin/core/party/dto/PartyUserInfo.java
+++ core/src/main/java/ru/rohtuasad/chipin/core/party/dto/PartyUserInfo.java
@@ -1,15 +1,19 @@
 package ru.rohtuasad.chipin.core.party.dto;
 
+import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.Data;
 import ru.rohtuasad.chipin.core.payment.model.Payment;
+import ru.rohtuasad.chipin.core.transfer.model.Transfer;
 import ru.rohtuasad.chipin.core.user.model.TgUser;
 
 @Data
 public class PartyUserInfo {
   private final TgUser user;
+  private BigDecimal total;
   private Set<Payment> paymentSet = new HashSet<>();
+  private Set<Transfer> transferSet = new HashSet<>();
   private Set<Debt> debt = new HashSet<>();
 
   public PartyUserInfo(TgUser user) {
--- core/src/main/java/ru/rohtuasad/chipin/core/party/service/PartyServiceImpl.java
+++ core/src/main/java/ru/rohtuasad/chipin/core/party/service/PartyServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.rohtuasad.chipin.core.party.service;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,6 +16,8 @@ import ru.rohtuasad.chipin.core.party.model.PartyUser;
 import ru.rohtuasad.chipin.core.party.repository.PartyRepository;
 import ru.rohtuasad.chipin.core.payment.model.Payment;
 import ru.rohtuasad.chipin.core.payment.service.PaymentService;
+import ru.rohtuasad.chipin.core.transfer.model.Transfer;
+import ru.rohtuasad.chipin.core.transfer.service.TransferService;
 import ru.rohtuasad.chipin.core.user.model.TgUser;
 import ru.rohtuasad.chipin.core.user.service.TgUserService;
 
@@ -24,6 +28,7 @@ public class PartyServiceImpl implements PartyService {
   private final PartyRepository partyRepository;
   private final TgUserService tgUserService;
   private final PaymentService paymentService;
+  private final TransferService transferService;
 
   @Override
   public Party createParty(@NonNull Long chatId) {
@@ -71,13 +76,41 @@ public class PartyServiceImpl implements PartyService {
     List<TgUser> users = tgUserService.getUsers(party);
     users.forEach(
         user -> partyInfo.getPartyUserInfoMap().put(user.getId(), new PartyUserInfo(user)));
+
+    final BigDecimal[] totalPayments = {BigDecimal.valueOf(0)};
     List<Payment> payments = paymentService.getPartyPayments(partyId);
     payments.forEach(payment -> {
       PartyUserInfo partyUserInfo = partyInfo.getPartyUserInfoMap().get(payment.getUserId());
       if (partyUserInfo != null) {
         partyUserInfo.getPaymentSet().add(payment);
+        partyUserInfo.setTotal(partyUserInfo.getTotal().add(payment.getAmount()));
+      }
+      totalPayments[0] = totalPayments[0].add(payment.getAmount());
+    });
+
+    List<Transfer> transfers = transferService.getPartyTransfers(partyId);
+    transfers.forEach(transfer -> {
+      PartyUserInfo senderUserInfo = partyInfo.getPartyUserInfoMap().get(transfer.getSenderId());
+      if (senderUserInfo != null) {
+        senderUserInfo.getTransferSet().add(transfer);
+        senderUserInfo.setTotal(senderUserInfo.getTotal().add(transfer.getAmount()));
+      }
+
+      PartyUserInfo receiverUserInfo = partyInfo.getPartyUserInfoMap()
+          .get(transfer.getReceiverId());
+      if (receiverUserInfo != null) {
+        receiverUserInfo.setTotal(receiverUserInfo.getTotal().subtract(transfer.getAmount()));
       }
     });
+
+    BigDecimal total = totalPayments[0];
+    BigDecimal middleValue = total.divide(
+        BigDecimal.valueOf(partyInfo.getPartyUserInfoMap().size()), MathContext.DECIMAL32);
+
+    partyInfo.getPartyUserInfoMap().values().forEach(user ->
+        user.setTotal(user.getTotal().subtract(middleValue))
+    );
+
     return partyInfo;
   }
 
